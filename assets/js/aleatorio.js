621803028d023d8cd99aebd7195c27724f92e6d2
LeidyRosmery <rosm3rita@hotmail.com>
2017-06-12 11:52:31 -0500

    funciona puntaje

--- assets/js/aleatorio.js
+++ assets/js/aleatorio.js
@@ -1,6 +1,9 @@
 var usados = [];
 var arraySede;
-var inputName=$('input');
+var numAleatorio;
+var sede;
+var puntajeTotal=0;
+var inputName = $('input');
 var contentCoder = $('.coder');
 var repetido = function(num) {
     var repe = false;
@@ -13,54 +16,60 @@ var repetido = function(num) {
     return repe;
 }
 var enteroAleatorio = function(min, max) {
-  if (usados.length!=(max-min)) {
-    while (repe != false) {
-      console.log("while");
-        var num = Math.floor(Math.random() * (max - min)) + min;
-        console.log(num);
-        var repe=repetido(num);
-    }
-    usados.push(num);
-    console.log(usados);
-    return num;
-  }else {
-    //var juego=;
-    contentCoder.append($('<p> el juego a terminado </p>'));
-    $('button').attr("disabled", true);
-    return null;
+    if (usados.length != (max - min)) {
+        while (repe != false) {
+            console.log("while");
+            var num = Math.floor(Math.random() * (max - min)) + min;
+            console.log(num);
+            var repe = repetido(num);
+        }
+        usados.push(num);
+        console.log(usados);
+        return num;
+    } else {
+        //var juego=;
+        contentCoder.append($('<p> el juego a terminado </p>'));
+        $('button').attr("disabled", true);
+        return null;
 
-  }
+    }
 }
 var seleccionSede = function(sede) {
     switch (sede) {
         case 'mexico':
             arraySede = mexico;
-          //  printFoto(sede);
+            //  printFoto(sede);
 
             break;
         case 'peru':
             arraySede = peru;
-          //  printFoto(sede);
+            //  printFoto(sede);
     }
 }
-$('select').change(function(e) {
 
+$('select').change(function(e) {
     var sede = $('.sede').val();
     seleccionSede(sede);
-    var numAleatorio=enteroAleatorio(1,6);
-    console.log(arraySede[numAleatorio].name);
-    console.log(sede);
+    numAleatorio = enteroAleatorio(1, 6);
     var imgCoder = $(`<img  class="imgCoder" alt="${arraySede[numAleatorio].name}" src="${'assets/img/'+sede+'/'+ arraySede[numAleatorio].image}" />`);
     contentCoder.append(imgCoder);
-    $('button').on('click', function(){
-      var numAleatorio=enteroAleatorio(1,6);
-      if (numAleatorio!= null && inputName.val()==arraySede[numAleatorio].name) {
-        console.log(sede);
-        var imgCoder = $(`<img  class="imgCoder" alt="${arraySede[numAleatorio].name}" src="${'assets/img/'+sede+'/'+ arraySede[numAleatorio].image}" />`);
-        contentCoder.append(imgCoder);
-      }else {
-        console.log("intenta");
-      }
-      //contentCoder.empty();
+     var contador=0;
+    $('button').on('click', function() {
+      contador++;
+        if ((numAleatorio != null && inputName.val()==arraySede[numAleatorio].name)||contador==5) {
+          contentCoder.empty();
+            numAleatorio = enteroAleatorio(1, 6);
+            var imgCoder = $(`<img  class="imgCoder" alt="${arraySede[numAleatorio].name}" src="${'assets/img/'+sede+'/'+ arraySede[numAleatorio].image}" />`);
+            contentCoder.append(imgCoder);
+            if(contador==5){
+              puntajeTotal--;
+            }else {
+              puntajeTotal++;
+            }
+            contador=0;
+            $('#puntajeTotal').text(puntajeTotal);
+
+        }
+        inputName.val("");
     });
 });
